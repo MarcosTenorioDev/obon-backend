eed84df793db4611865135e4bf0e46488e1c10da
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-11-13 12:49:30 -0300

    Fix: fix SonarQube warnings

--- src/utils/s3.utils.ts
+++ src/utils/s3.utils.ts
@@ -1,47 +1,51 @@
-import S3 from 'aws-sdk/clients/s3';
-import path from 'path';
-import multerLib from '../lib/multer.lib';
-import mime from 'mime';
-import fs from 'fs';
-import { env } from '../env';
+import S3 from "aws-sdk/clients/s3";
+import fs from "fs";
+import mime from "mime";
+import path from "path";
+import { env } from "../env";
+import multerLib from "../lib/multer.lib";
 class S3Storage {
-    private client: S3;
+	private readonly client: S3;
 
-    constructor(){
-        this.client = new S3({
-            region: env.AWS_REGION,
-            accessKeyId: env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: env.AWS_SECRET_ACCESS_KEY
-        });
-    }
+	constructor() {
+		this.client = new S3({
+			region: env.AWS_REGION,
+			accessKeyId: env.AWS_ACCESS_KEY_ID,
+			secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+		});
+	}
 
-    async saveFile(filename: string): Promise<any> {
-        const originalPath = path.resolve(multerLib.directory, filename);
+	async saveFile(filename: string): Promise<any> {
+		const originalPath = path.resolve(multerLib.directory, filename);
 
-        const ContentType = mime.getType(originalPath);
+		const ContentType = mime.getType(originalPath);
 
-        if(!ContentType){
-            throw new Error('File not found');
-        }
+		if (!ContentType) {
+			throw new Error("File not found");
+		}
 
-        const fileContent = await fs.promises.readFile(originalPath);
+		const fileContent = await fs.promises.readFile(originalPath);
 
-        await this.client.putObject({
-            Bucket: env.AWS_BUCKET as string, 
-            Key: filename,
-            ACL: 'public-read',
-            Body: fileContent,
-            ContentType
-        }).promise();
+		await this.client
+			.putObject({
+				Bucket: env.AWS_BUCKET as string,
+				Key: filename,
+				ACL: "public-read",
+				Body: fileContent,
+				ContentType,
+			})
+			.promise();
 
-        await fs.promises.unlink(originalPath);
-    }
-    async deleteFile(filename: string): Promise<void> {
-        await this.client.deleteObject({
-            Bucket: env.AWS_BUCKET as string,
-            Key: filename
-        }).promise();
-    }
+		await fs.promises.unlink(originalPath);
+	}
+	async deleteFile(filename: string): Promise<void> {
+		await this.client
+			.deleteObject({
+				Bucket: env.AWS_BUCKET as string,
+				Key: filename,
+			})
+			.promise();
+	}
 }
 
 export default S3Storage;
